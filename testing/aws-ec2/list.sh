a4930bfac978d081417328d00f27ae0476965e82
mtilson <m@tilson.biz>
2022-10-15 21:32:54 +0300

    add .ssh/rc to ubuntu user

--- testing/aws-ec2/list.sh
+++ testing/aws-ec2/list.sh
@@ -14,7 +14,7 @@ tag_type=${tag_type:-mtilson/user-data}
 
 aws ec2 describe-instances \
   --filters Name=tag:Type,Values=$tag_type Name=instance-state-name,Values=running \
-  --query 'Reservations[].Instances[].{InstanceId:InstanceId, TAGS:Tags[?Key == `Name`].Value, IP:NetworkInterfaces[].Association.PublicIp}' | \
+  --query 'Reservations[].Instances[].{InstanceId:InstanceId, TAGS:Tags[?Key == `OS`].Value, IP:NetworkInterfaces[].Association.PublicIp}' | \
   jq -r '.[] | [.InstanceId, .TAGS[], .IP[]] | @csv' | tr -d '"' | \
   while read line ; do
     id=$(echo $line | cut -d"," -f1)
